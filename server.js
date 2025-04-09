ecf44fad0641dd5b298294139b05db8e089c4472
hungbin <bincoitk31@gmail.com>
2025-04-09 21:26:55 +0700

    test

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.post('/api/run_command', (req, res) => {
   process.env.VITE_ZALO_OA_ID = zalo_oa_id
   process.env.VITE_APP_ID = app_id
   process.env.VITE_ZALO_PRIVATE_KEY = zalo_private_key
-  process.env.VITE_ENV = env
+  process.env.VITE_ENV = "STAGING"
 
   //write settings to app-settings.json
 
