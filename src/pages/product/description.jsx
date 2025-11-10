a8e033a961e4ce75d1cdd6724610a21c0ca76b21
hungbin <bincoitk31@gmail.com>
2025-11-10 14:46:59 +0700

    Fix

--- src/pages/product/description.jsx
+++ src/pages/product/description.jsx
@@ -1,13 +1,13 @@
 import { resizeDescription } from "../../utils/tools"
 const Description = ({product}) => {
+  const convertDes = (des) => {
+    return des.split(/\r?\n/).map((line) => line.trim()).filter(Boolean).join("<br />")
+  }
+
   return (
     <div className="">
       <div className="font-bold py-3">Chi tiết sản phẩm</div>
-     
-      {/* <div className="default-style" dangerouslySetInnerHTML={{__html: resizeDescription(product.description)}}></div> */}
-      <div className="default-style">
-        <p dangerouslySetInnerHTML={{__html: resizeDescription(product.description)}}></p>
-      </div>
+      <div className="default-style" dangerouslySetInnerHTML={{__html: /^\<.+\>/.test(product.description) ? resizeDescription(product.description) : convertDes(product.description)}}></div>
     </div>
   )
 }
